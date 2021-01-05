91d307383350d4c8d4a4594e653e7fd2b6a793bb
Álvaro Torres Cogollo <atorrescogollo@gmail.com>
2021-01-05 01:32:55 +0100

    Grow extended partitions before primaries

--- files/grow_pv.sh
+++ files/grow_pv.sh
@@ -10,6 +10,16 @@ set -e
 pvdisks=$( pvs --noheadings | awk '{print $1}' | sed 's/^\(.*\)[0-9]\+/\1/g' | sort -du )
 while read -r disk_dev # /dev/sda
 do
+    # Grow extended partitions
+    fdisk -l "$disk_dev" | grep 'Extended$' | awk '{print $1}' | while read extended
+    do
+        extnum=$(echo "$extended"| grep -oP '[0-9]+$')
+        (
+            set -x
+            growpart "$disk_dev" "$extnum" || true
+        )
+    done
+
     partition_num=$( pvs --noheadings | fgrep "$disk_dev" | sort | tail -1 | awk '{print $1}' | sed 's/^.*\([0-9]\+\)/\1/g')
     (
         set -x
